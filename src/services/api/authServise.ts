8958c46dc13765e99e7be23d2100ded60a5dac66
Mahla Parvaz <mahlaparvaz98@gmail.com>
2025-03-10 15:46:46 +0330

    feat: auth context added

---
+++ src/services/api/authServise.ts
@@ -0,0 +1,20 @@
+import http from '../core/httpServices';
+
+export async function signinApi(data) {
+  return http.post(`/user/signin`, data).then(({ data }) => data);
+}
+
+export async function signupApi(data) {
+  return http.post(`/user/signup`, data).then(({ data }) => data);
+}
+
+export async function getUserApi(options) {
+  return http.get(`/user/profile`, options).then(({ data }) => data);
+}
+export async function getAllUsersApi(options) {
+  return http.get(`/user/list`, options).then(({ data }) => data.data);
+}
+
+export function logoutApi() {
+  return http.post(`/user/logout`);
+}
